3648111445b30163ab52c68279598233dea26c44
Vikas Verma <vermavikas1002@gmail.com>
2021-05-08 23:44:06 +0530

    test cases included

---
+++ src/test/java/com/hcl/webapp6/AppTest.java
@@ -0,0 +1,33 @@
+package com.hcl.webapp6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( true );
+    }    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+}
